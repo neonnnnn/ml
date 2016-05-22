52c74887cd7c806e4ebd5c940f68e1944351406c
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2016-05-22 14:27:17 +0900

    rename SVM to svm

---
+++ svm/kernel.py
@@ -0,0 +1,92 @@
+from __future__ import division
+import utils
+import numpy as np
+
+
+class Linear(object):
+    def __init__(self, params=None):
+        pass
+
+    @staticmethod
+    def calc_kernel(x1, x2):
+        return np.dot(x1, x2.T).T
+
+    @staticmethod
+    def calc_kernel_same(x1):
+        return np.sum(x1 * x1, axis=1)
+
+
+class SparseLinear(object):
+    def __init__(self, params=None):
+        pass
+
+    @staticmethod
+    def calc_kernel(x1, x2):
+        return x1.dot(x2.T).T.toarray()
+
+    @staticmethod
+    def calc_kernel_same(x1):
+        return np.array(x1.multiply(x1).sum(axis=1)).ravel()
+
+
+class Polynomial(object):
+    def __init__(self, params):
+        self.d = params[0]
+        self.c = params[1]
+
+    def calc_kernel(self, x1, x2):
+        return (np.dot(x1, x2.T).T + self.c) ** self.d
+
+    def calc_kernel_same(self, x1):
+        return (np.sum(x1*x1, axis=1) + self.c) ** self.d
+
+
+class SparsePolynomial(object):
+    def __init__(self, params):
+        self.d = params[0]
+        self.c = params[1]
+
+    def calc_kernel(self, x1, x2):
+        return (x1.dot(x2.T).T.toarray() + self.c) ** self.d
+
+    def calc_kernel_same(self, x1):
+        return (np.array(x1.multiply(x1).sum(axis=1)).ravel() + self.c) ** self.d
+
+
+class RBF(object):
+    def __init__(self, params):
+        self.gamma = params[0]
+
+    def calc_kernel(self, x1, x2):
+        x2 = np.atleast_2d(x2)
+        return np.exp(- self.gamma * (np.atleast_2d(np.sum(x1 * x1, axis=1)) + np.atleast_2d(np.sum(x2 * x2, axis=1)).T - 2 * np.dot(x1, x2.T).T))
+
+    @staticmethod
+    def calc_kernel_same(x1):
+        return np.ones(x1.shape[0])
+
+
+class SparseRBF(object):
+    def __init__(self, params):
+        self.gamma = params[0]
+
+    def calc_kernel(self, x1, x2):
+        norm1 = np.array(x1.multiply(x1).sum(axis=1)).ravel()
+        norm2 = np.array(x2.multiply(x2).sum(axis=1)).ravel()
+        return np.exp(- self.gamma * (np.ones([x2.shape[0], 1]) * norm1 + (np.ones([x1.shape[0], 1]) * norm2).T - 2 * x1.dot(x2.T).T.toarray()))
+
+    @staticmethod
+    def calc_kernel_same(x1):
+        return np.ones(x1.shape[0])
+
+
+def get_kernel(identifier):
+    return utils.get_from_module(identifier, globals(), 'kernel')
+
+linear = Linear
+Sparselinear = SparseLinear
+polynomial = Polynomial
+Sparsepolynomial = SparsePolynomial
+rbf = RBF
+Sparserbf = SparseRBF
+
---
+++ svm/svc.py
@@ -0,0 +1,189 @@
+import numpy as np
+import kernel
+import sys
+
+
+class SVC(object):
+    def __init__(self, C, kernel_name='linear', params=None, eps=1e-4, tau=1e-12, max_iter=10000, wss='WSS3', sparse=False):
+        self.C = C
+        if sparse:
+            self.K = kernel.get_kernel("Sparse" + kernel_name)(params)
+        else:
+            self.K = kernel.get_kernel(kernel_name)(params)
+        self.params = params
+        self.eps = eps
+        self.tau = tau
+        self.max_iter = max_iter
+        self.WSS = self.__getattribute__(wss)
+        self.alpha = None
+        self.support_vector = None
+        self.bias = 0
+        self.alpha_times_y = None
+        self.cache = {}
+        self.cache2 = None
+        self.keys = None
+        self.flag = False
+
+    def decision_function(self, x):
+        return (self.K.calc_kernel(self.support_vector, x)).dot(self.alpha_times_y) + self.bias
+
+    def predict(self, x):
+        return np.sign(self.decision_function(x))
+
+    def score(self, x, y):
+        pred = self.predict(x)
+        return 1.0 * sum(pred * y > 0) / len(pred)
+
+    def _working_set_selection1(self, grad_f_a, up_idx, low_idx, x, y):
+        minus_y_times_grad = - y * grad_f_a
+        idx1 = up_idx[np.argmax(minus_y_times_grad[up_idx])]
+        idx2 = low_idx[np.argmin(minus_y_times_grad[low_idx])]
+        y1 = y[idx1]
+        y2 = y[idx2]
+        if not (idx1 in self.cache):
+            self.cache[idx1] = y * y1 * (self.K.calc_kernel(x, x[idx1])).ravel()
+
+        if not (idx2 in self.cache):
+            self.cache[idx2] = y * y2 * (self.K.calc_kernel(x, x[idx2])).ravel()
+
+        # convergence test
+        if minus_y_times_grad[idx1] - minus_y_times_grad[idx2] < self.eps:
+            self.flag = True
+
+        a = (self.cache[idx1])[idx1] + (self.cache[idx2])[idx2] - 2 * y1 * y2 * (self.cache[idx1])[idx2]
+        if a <= 0:
+            a = self.tau
+        d = (minus_y_times_grad[idx1] - minus_y_times_grad[idx2]) / a
+
+        return idx1, idx2, d
+
+    def _working_set_selection3(self, grad_f_a, up_idx, low_idx, x, y):
+        minus_y_times_grad = - y * grad_f_a
+        idx1 = up_idx[np.argmax(minus_y_times_grad[up_idx])]
+        t = low_idx[np.where(minus_y_times_grad[low_idx] < minus_y_times_grad[idx1])]
+        y1 = y[idx1]
+
+        if not (idx1 in self.cache):
+            self.cache[idx1] = y * y1 * self.K.calc_kernel(x, x[idx1]).ravel()
+        Qii = self.cache[idx1]
+
+        newkeys = t[self.keys[t]]
+        if len(newkeys) > 0:
+            self.keys[newkeys] = False
+            self.cache2[newkeys] = self.K.calc_kernel_same(x[newkeys])
+        Qtt = self.cache2[t]
+
+        ait = Qii[idx1] + Qtt - 2 * y1 * y[t] * Qii[t]
+        ait[np.where(ait <= 0)] = self.tau
+        bit = minus_y_times_grad[idx1] - minus_y_times_grad[t]
+        obj_min = - (bit ** 2) / ait
+        t_idx = np.argmin(obj_min)
+        idx2 = t[t_idx]
+
+        # convergence test
+        if minus_y_times_grad[idx1] - minus_y_times_grad[idx2] < self.eps:
+            self.flag = True
+
+        if not (idx2 in self.cache):
+            self.cache[idx2] = y * y[idx2] * self.K.calc_kernel(x, x[idx2]).ravel()
+
+        return idx1, idx2, bit[t_idx] / ait[t_idx]
+
+    def check_up_idx(self, y, alpha):
+        if ((y == 1) and (alpha < self.C)) or ((y == -1) and (alpha > 0)):
+            return True
+        else:
+            return False
+
+    def check_low_idx(self, y, alpha):
+        if ((y == 1) and (alpha > 0)) or ((y == -1) and (alpha < self.C)):
+            return True
+        else:
+            return False
+
+    def init_params(self, y):
+        self.alpha = np.zeros(y.shape[0])
+        self.cache2 = np.zeros(y.shape[0])
+        self.keys = np.ones(y.shape[0], dtype=bool)
+        if np.min(y) == 0 and np.max(y) == 1:
+            y = 2*y - 1
+        return y, y > 0, y < 0, - np.ones(y.shape[0])
+
+    def calc_alpha(self, y1, y2, alpha1_old, alpha2_old, d):
+        alpha1_new = alpha1_old + y1 * d
+        const = y1 * alpha1_old + y2 * alpha2_old
+
+        if alpha1_new > self.C:
+            alpha1_new = self.C
+        elif alpha1_new < 0:
+            alpha1_new = 0
+
+        alpha2_new = y2 * (const - y1 * alpha1_new)
+
+        if alpha2_new > self.C:
+            alpha2_new = self.C
+            alpha1_new = y1 * (const - y2 * alpha2_new)
+        elif alpha2_new < 0:
+            alpha2_new = 0
+            alpha1_new = y1 * const
+
+        return alpha1_new, alpha2_new
+
+    def calc_result(self, x, y, grad_f_a):
+        support_vector_idx = np.where(self.alpha > 1e-5 * self.C)
+        self.support_vector = x[support_vector_idx]
+        self.alpha = self.alpha[support_vector_idx]
+        self.alpha_times_y = self.alpha * y[support_vector_idx]
+        self.bias = np.average(-y[support_vector_idx] * grad_f_a[support_vector_idx])
+
+    def fit(self, x, y):
+        print ("training ...")
+
+        # init_params
+        y, up_idx, low_idx, grad_f_a = self.init_params(y)
+
+        for i in range(self.max_iter):
+            if i % (self.max_iter / 1000):
+                sys.stdout.write("\r Iteration:%d/%d" % (i+1, self.max_iter))
+                sys.stdout.flush()
+            # select working set
+            idx1, idx2, d = self.WSS(grad_f_a, up_idx.nonzero()[0], low_idx.nonzero()[0], x, y)
+
+            y1 = y[idx1]
+            y2 = y[idx2]
+            Qii = self.cache[idx1]
+            Qjj = self.cache[idx2]
+            alpha1_old = self.alpha[idx1]
+            alpha2_old = self.alpha[idx2]
+
+            # update alpha_1 and alpha_2
+            alpha1_new, alpha2_new = self.calc_alpha(y1, y2, alpha1_old, alpha2_old, d)
+
+            # update alpha
+            self.alpha[idx1] = alpha1_new
+            self.alpha[idx2] = alpha2_new
+
+            # update grad_f_a
+            grad_f_a += Qii * (alpha1_new - alpha1_old) + Qjj * (alpha2_new - alpha2_old)
+
+            # update up_idx
+            up_idx[idx1] = self.check_up_idx(y1, alpha1_new)
+            up_idx[idx2] = self.check_up_idx(y2, alpha2_new)
+
+            # update low_idx
+            low_idx[idx1] = self.check_low_idx(y1, alpha1_new)
+            low_idx[idx2] = self.check_low_idx(y2, alpha2_new)
+
+            if self.flag:
+                print("\nConverge.")
+                break
+        if not self.flag:
+            print ("")
+        print ('Training Complete.\nIteration:'), (i+1)
+        print ('len(cache):'), len(self.cache)
+        self.cache.clear()
+        del self.cache2
+        self.calc_result(x, y, grad_f_a)
+
+    WSS1 = wss1 = _working_set_selection1
+    WSS3 = wss3 = _working_set_selection3
