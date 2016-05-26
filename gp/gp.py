f0895fe026626af66e12487008a8fd50aa81675f
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2016-05-26 18:30:00 +0900

    Change optimizer(gp), fixed Matern, and add SE(kernel).

--- gp/gp.py
+++ gp/gp.py
@@ -1,11 +1,12 @@
 import numpy as np
 import kernel
 import scipy.optimize as spopt
+import scipy.linalg
 
 
 class GP(object):
     def __init__(self, kernel_name="Matern52", alpha=None, beta=None, theta=None, type2ml=True):
-        self.K = kernel.get_kernel(kernel_name)(theta)
+        self.K = kernel.get_kernel(kernel_name)()
         self.theta = theta
         self.alpha = alpha
         self.beta = beta
@@ -13,14 +14,20 @@ class GP(object):
         self.t = None
         self.x = None
         self.C = None
+	self.cholesky_C = None
 
     def negative_log_likelihood(self, params):
         alpha = np.exp(params[0])
         beta = np.exp(params[1])
         theta = np.exp(params[2:])
         C = self.K.calc_kernel(self.x, self.x, theta) * alpha + np.identity(self.x.shape[0]) * beta
-        inv_C_times_t = np.linalg.solve(C, self.t)
-        return 0.5 * np.log(np.linalg.det(C) + 1e-30) + 0.5 * np.dot(inv_C_times_t, self.t) + 0.5 * self.t.shape[0] * np.log(2 * np.pi)
+
+        cholesky_C = scipy.linalg.cholesky(C, lower=True)
+        inv_C_times_t = scipy.linalg.cho_solve((cholesky_C, True), self.t)
+
+        # det(C) = det(Cholesky) * det(Cholesky)
+        # log(det(C)) = 2 log(det(Cholesky))= 2 log(\prod_{i=1}^{N}Cholesky_{ii}) = 2 \sum_{i=1}^{N}log(Cholesky_{ii})
+        return np.sum(np.log(np.diag(cholesky_C))) + 0.5 * np.dot(inv_C_times_t, self.t) + 0.5 * self.t.shape[0] * np.log(2 * np.pi)
 
     def grad_nll(self, params):
         alpha = np.exp(params[0])
@@ -29,7 +36,9 @@ class GP(object):
 
         K = self.K.calc_kernel(self.x, self.x, theta)
         C = alpha * K + beta * np.identity(self.x.shape[0])
-        inv_C = np.linalg.inv(C)
+        cholesky_C = scipy.linalg.cholesky(C, lower=True)
+
+        inv_C = scipy.linalg.cho_solve((cholesky_C, True), np.eye(cholesky_C.shape[0]))
         t_times_inv_C = np.dot(inv_C, self.t)
         grad_C_alpha = K * alpha
         grad_C_theta = self.K.calc_grad(self.x, self.x, theta) * alpha
@@ -43,7 +52,7 @@ class GP(object):
 
         return grad
 
-    def fit(self, x, y, hyper_opt_iter=30):
+    def fit(self, x, y, hyper_opt_iter=10, bounds=None):
         print ("training...")
         if len(x.shape) == 1:
             dim = 1
@@ -59,10 +68,14 @@ class GP(object):
         if self.type2ml:
             print (" Doing Type 2 Maximum Likelihood...")
             f_min = np.inf
-            for i in xrange(hyper_opt_iter):
-                init_params = np.random.rand(self.K.dim + 2)
-                res = spopt.fmin_cg(self.negative_log_likelihood, init_params, fprime=self.grad_nll, full_output=True, disp=False)
+            if bounds is None:
+                bounds = [(-5, 5), (-5, 5)]
+                for i in xrange(self.K.dim):
+                    bounds.append((-5, 5))
 
+            for i in xrange(hyper_opt_iter):
+                init_params = np.random.rand(self.K.dim + 2) * 2 - 1
+                res = spopt.fmin_l_bfgs_b(self.negative_log_likelihood, init_params, self.grad_nll, disp=False, iprint=0, bounds=bounds)
                 if res[1] < f_min:
                     f_min = res[1]
                     params = res[0]
@@ -70,10 +83,12 @@ class GP(object):
             self.alpha = np.exp(params[0])
             self.beta = np.exp(params[1])
             self.theta = np.exp(params[2:])
+            self.K.theta = self.theta
 
         # number of train data = N : x.shape[0]
         # C.shape : (N, N)
         self.C = self.alpha * self.K.calc_kernel(self.x, self.x, self.theta) + self.beta * np.identity(self.x.shape[0])
+        self.cholesky_C = scipy.linalg.cholesky(self.C, lower=True)
 
     def decision_function(self, x):
         if len(x.shape) == 1:
@@ -83,14 +98,10 @@ class GP(object):
         # k.shape = (M,N)
         k = self.K.calc_kernel(self.x, x, self.theta) * self.alpha
         # c.shape = (M,)
-        if len(x.shape) == 1:
-            c = self.alpha * self.K.calc_kernel_same(x) + 1.0 * self.beta
-        else:
-            c = self.alpha * self.K.calc_kernel_diag(x) + 1.0 * self.beta
+        c = self.alpha * self.K.calc_kernel_diag(x) + 1.0 * self.beta
 
         # Trans_k_dot_inv_C.shape = (M,N)
-        Trans_k_dot_inv_C = np.linalg.solve(self.C, k.T).T
-
+        Trans_k_dot_inv_C = np.linalg.cho_solve((self.cholesky_C, True), k.T).T
         # mean.shape = (M, )
         mean = np.dot(Trans_k_dot_inv_C, self.t)
         # var.shape = (M,)
--- gp/kernel.py
+++ gp/kernel.py
@@ -14,44 +14,68 @@ def calc_r2(x1, x2, theta):
     return np.where(r2 < 0, 0, r2)
 
 
-class Matern52(object):
+class SE(object):
     def __init__(self, theta=None):
         self.theta = theta
-        self.dim = 0
+        self.dim = 1
         self.ard = True
 
     def calc_kernel(self, x1, x2, theta=None):
         if theta is None:
             theta = self.theta
-        r2 = calc_r2(x1, x2, theta)
-
-        r = np.sqrt(r2)
-        return (1 + np.sqrt(5) * r + 5. * r2 / 3) * np.exp(-np.sqrt(5) * r)
-
-    @staticmethod
-    def calc_kernel_same(x1):
-        return 1.0
-
-    @staticmethod
-    def calc_kernel_diag(x1):
-        return np.ones(x1.shape[0])
+        r2 = calc_r2(x1, x2, theta[1:])
+        return theta[0] * np.exp(-0.5 * r2)
 
     def calc_grad(self, x1, x2, theta):
         n = x1.shape[0]
-        grad_r2 = np.zeros((x1.shape[1], n, n))
-        for i in range(self.dim):
-            grad_r2[i] = theta[i] * (x1[:, i] - (x1[:, i])[:, np.newaxis]) ** 2
-        grad_r2 += 1e-10
+        grad_r2 = np.zeros((self.dim, n, n))
+        for i in range(1, self.dim):
+            grad_r2[i] = theta[i] * (x1[:, i-1] - (x1[:, i-1])[:, np.newaxis]) ** 2
+        grad_r2[0]
 
-        r2 = calc_r2(x1, x2, theta)
+        r2 = calc_r2(x1, x2, theta[1:])
+        grad = -0.5 * grad_r2 * np.exp(-0.5 * r2) * theta[0]
+        grad[0] = np.exp(-0.5 * r2) * theta[0]
+        return grad
+
+    def calc_kernel_diag(self, x1):
+        return self.theta[0] * np.ones(x1.shape[0])
+
+
+class Matern52(object):
+    def __init__(self, theta=None):
+        self.theta = theta
+        self.dim = 1
+        self.ard = True
+
+    def calc_kernel(self, x1, x2, theta=None):
+        if theta is None:
+            theta = self.theta
+        r2 = calc_r2(x1, x2, theta[1:])
+
+        r = np.sqrt(r2)
+        return theta[0] * (1 + np.sqrt(5) * r + 5. * r2 / 3) * np.exp(-np.sqrt(5) * r)
+
+    def calc_kernel_diag(self, x1):
+        return np.ones(x1.shape[0]) * self.theta[0]
+
+    def calc_grad(self, x1, x2, theta):
+        n = x1.shape[0]
+        grad_r2 = np.zeros((self.dim, n, n))
+        for i in range(1, self.dim):
+            grad_r2[i] = theta[i] * (x1[:, i-1] - (x1[:, i-1])[:, np.newaxis]) ** 2
+
+        r2 = calc_r2(x1, x2, theta[1:])
         r = np.sqrt(r2)
         inv_r = 1. / np.where(r != 0, r, np.inf)
         sqrt5 = np.sqrt(5)
-        grad1 = (sqrt5 * grad_r2 * inv_r / 2. + 5. * grad_r2 / 3.)
-        grad2 = (-sqrt5 * grad_r2 * inv_r / 2.) * (1 + sqrt5 * r + 5. * r2 / 3.)
+        grad = (-0.5 * sqrt5 * grad_r2 * inv_r * (sqrt5 * r + 5. * r2 / 3.) + 5. * grad_r2 / 3.)
+        grad[0] = (1 + np.sqrt(5) * r + 5. * r2 / 3)
 
-        return (grad1 + grad2) * np.exp(-sqrt5 * r2)
+        return theta[0] * grad * np.exp(-sqrt5 * r)
 
 
+def get_kernel(identifier):
+    return utils.get_from_module(identifier, globals(), 'kernel')
 
-matern52=Matern52
+matern52 = Matern52
