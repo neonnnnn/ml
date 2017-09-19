f72a3f412e13f90b3f3733cdaa2960b45570afa7
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2017-09-20 02:57:35 +0900

    curry methods

--- ml/deeplearning/initializations.py
+++ ml/deeplearning/initializations.py
@@ -3,37 +3,49 @@ import numpy as np
 from .. import utils
 
 
-def uniform(layer, shape, scale=0.05):
-    return layer.rng.uniform(low=-scale, high=scale, size=shape)
+def uniform(scale=0.05):
+    def _uniform(layer, shape):
+        return layer.rng.uniform(low=-scale, high=scale, size=shape)
+    return _uniform
 
 
-def normal(layer, shape, mu=0., sigma=0.02):
-    return layer.rng.normal(mu, sigma, shape)
+def normal(mu=0., sigma=0.001):
+    def _normal(layer, shape):
+        return layer.rng.normal(mu, sigma, shape)
+    return _normal
 
 
-def zero(layer, shape):
-    return np.zeros(shape)
+def zero():
+    def _zero(layer, shape):
+        return np.zeros(shape)
+    return _zero
 
 
-def glorot_uniform(layer, shape, coeff=6.):
-    if len(shape) == 2:
-        scale = np.sqrt(coeff / sum(shape))
-    else:
-        fan_in = np.prod(layer.filter_shape[1:])
-        fan_out = layer.filter_shape[0] * np.prod(layer.filter_shape[2:])
-        scale = np.sqrt(coeff / (fan_in + fan_out))
-    return uniform(layer, shape, scale)
+def glorot_uniform(coeff=6.):
+    def _glorot_uniform(layer, shape):
+        if len(shape) == 2:
+            scale = np.sqrt(coeff / sum(shape))
+        else:
+            fan_in = np.prod(layer.filter_shape[1:])
+            fan_out = layer.filter_shape[0] * np.prod(layer.filter_shape[2:])
+            scale = np.sqrt(coeff / (fan_in + fan_out))
+        return uniform(scale)(layer, shape)
+    return _glorot_uniform
 
 
-def lecun_uniform(layer, shape):
-    scale = np.sqrt(3. / layer.fan_in)
-    return uniform(layer, shape, scale)
+def lecun_uniform():
+    def _lecun_uniform(layer, shape):
+        scale = np.sqrt(3. / layer.fan_in)
+        return uniform(scale)(layer, shape)
+    return _lecun_uniform
 
 
-def he_conv_normal(layer, shape):
-    mu = 0
-    sigma = np.sqrt(2. / (layer.n_in[0] * (layer.filter_shape[0] ** 2)))
-    return normal(layer, shape, mu, sigma)
+def he_conv_normal():
+    def _he_conv_normal(layer, shape):
+        mu = 0
+        sigma = np.sqrt(2. / (layer.n_in[0] * (layer.filter_shape[0] ** 2)))
+        return normal(mu, sigma)(layer, shape)
+    return _he_conv_normal
 
 
 def get_init(identifier):
