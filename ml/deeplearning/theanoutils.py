704ec7b2297aa3208783e3e940111b030876360b
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2017-09-28 23:35:04 +0900

    fix a bit

--- ml/deeplearning/theanoutils.py
+++ ml/deeplearning/theanoutils.py
@@ -8,24 +8,18 @@ from theano.compile.sharedvalue import SharedVariable
 
 
 def sharedzeros(shape, name=None):
-    return theano.shared(np.zeros(shape, dtype=theano.config.floatX),
-                         borrow=True,
-                         name=name)
+    return theano.shared(np.zeros(shape, dtype=theano.config.floatX), borrow=True, name=name)
 
 
 def sharedones(shape, name=None):
-    return theano.shared(np.ones(shape, dtype=theano.config.floatX),
-                         borrow=True,
-                         name=name)
+    return theano.shared(np.ones(shape, dtype=theano.config.floatX), borrow=True, name=name)
 
 
 def sharedasarray(input, name=None):
     if input is None or isinstance(input, SharedVariable):
         return input
     else:
-        return theano.shared(np.asarray(input, dtype=theano.config.floatX),
-                             borrow=True,
-                             name=name)
+        return theano.shared(np.asarray(input, dtype=theano.config.floatX), borrow=True, name=name)
 
 
 def variable(x, name=None):
@@ -49,8 +43,7 @@ def run(inputs, function, iprint=True):
         s = timeit.default_timer()
         i = 0
     for batch in inputs:
-        output += function(*[b if not sp.issparse(b) else b.toarray()
-                             for b in batch])
+        output += function(*[b if not sp.issparse(b) else b.toarray() for b in batch])
         if iprint:
             e = timeit.default_timer()
             progbar(i+1, n_batches, e-s)
@@ -59,8 +52,7 @@ def run(inputs, function, iprint=True):
 
 
 def run_on_batch(inputs, function):
-    output = function(*[b if not sp.issparse(b) else b.toarray()
-                        for b in inputs])
+    output = function(*[b if not sp.issparse(b) else b.toarray() for b in inputs])
     return output
 
 
