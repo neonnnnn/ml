28d23da30f84eabfd3a2d5e6248e269d5ac9b23e
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2017-12-04 01:25:17 +0900

    add alternative recursion for coordinate descent of hofm

--- ml/fm/anova.py
+++ ml/fm/anova.py
@@ -10,8 +10,42 @@ def anova(p, x, order, dptable):
 def _anova(p, x, order, d, a):
     for t in range(1, order+1):
         for j in range(t, d+1):
-            a[t, j] = a[t, j-1] + p[j-1]*x[j-1]*a[t-1, j-1] 
+            a[t, j] = a[t, j-1] + p[j-1]*x[j-1]*a[t-1, j-1]
     return a[t, j]
+
+def anova_saving_memory(p, x, order, dptable):
+    dptable[0] = 1
+    dptable[1:] = 0
+    d = p.shape[0]
+    return _anova_saving_memory(p, x, order, d, dptable)
+
+
+def _anova_saving_memory(p, x, order, d, a):
+    for j in range(order):
+        for t in range(j+1):
+            a[j+1-t] += a[j-t] * p[j]*x[j]
+    for j in range(order, d):
+        for t in range(order):
+            a[order-t] += a[order-t-1]*p[j]*x[j]
+
+    return a[order]
+
+def anova_alt(p, x, order, dptable_anova, dptable_poly):
+    dptable_anova[:, 0] = 1
+    dptable_poly[:, 0] = 1
+    return _anova_alt(p, x, order, dptable_anova, dptable_poly)
+
+
+def _anova_alt(p, x, order, a, poly):
+    for m in range(1, order+1):
+        temp = 0.
+        sign = 1.
+        for t in range(1, m+1):
+            temp += sign * a[:, m-t]*poly[:, t]
+            sign *= -1
+        a[:, m] = temp / m
+
+    return a[:, order]
 
 
 def grad_anova(p, x, order, dptable_anova, dptable_grad):
@@ -27,3 +61,22 @@ def _grad_anova(p, x, order, d, a, grad):
             grad[t-1, j-1] = grad[t-1, j] + grad[t, j]*p[j]*x[j]
 
     return np.sum(a[:order, :-1] * grad[:order], axis=0) * x
+
+
+def grad_anova_alt(p_js, x_j, order, dptable_anova, dptable_poly, dptable_grad):
+    dptable_grad[:, 0] = 0
+    dptable_grad[:, 1] = x_j
+    return _grad_anova_alt(p_js, x_j, order, dptable_anova, dptable_poly, dptable_grad)
+
+
+def _grad_anova_alt(p, x, order, a, poly, grad):
+    for m in range(2, order+1):
+        temp = 0
+        sign = 1
+        for t in range(1, m+1):
+            temp += (grad[:, m-t]*poly[:, t] + a[:, m-t]*t*(p**(t-1))*(x**t)) * sign
+            sign *= -1
+        grad[:, m] = temp / m
+
+    return grad[:, order]
+
